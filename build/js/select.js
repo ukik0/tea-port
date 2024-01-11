ad8e0831ada6cc60b28f2977832b960bc9c275e0
vyazemskaya <vyazemskayanna@gmail.com>
2024-01-12 00:00:00 +0300

    update

--- build/js/select.js
+++ build/js/select.js
@@ -2,23 +2,23 @@
 const selectFathers = document.querySelectorAll('.select');
 
 const setSelects = () => {
-	if (selectFathers) {
-		selectFathers.forEach((select) => {
-			const childs = select.querySelectorAll('.select-item');
+    if (selectFathers) {
+        selectFathers.forEach((select) => {
+            const childs = select.querySelectorAll('.select-item');
 
-			childs.forEach((current) => {
-				current.addEventListener('click', () => {
-					childs.forEach((item) => {
-						if (current === item) {
-							current.classList.add(activeClass);
-						} else {
-							item.classList.remove(activeClass);
-						}
-					});
-				});
-			});
-		});
-	}
+            childs.forEach((current) => {
+                current.addEventListener('click', () => {
+                    childs.forEach((item) => {
+                        if (current === item) {
+                            current.classList.add('active');
+                        } else {
+                            item.classList.remove('active');
+                        }
+                    });
+                });
+            });
+        });
+    }
 };
 
 document.addEventListener('DOMContentLoaded', setSelects);
@@ -27,23 +27,23 @@ document.addEventListener('DOMContentLoaded', setSelects);
 const selectFatherClose = document.querySelectorAll('.select-close');
 
 const setCloseSelects = () => {
-	if (selectFatherClose) {
-		selectFatherClose.forEach((select) => {
-			const childs = select.querySelectorAll('.select-item');
+    if (selectFatherClose) {
+        selectFatherClose.forEach((select) => {
+            const childs = select.querySelectorAll('.select-item');
 
-			childs.forEach((current) => {
-				current.addEventListener('click', () => {
-					childs.forEach((item) => {
-						if (current === item && !item.classList.contains('active')) {
-							current.classList.add(activeClass);
-						} else {
-							item.classList.remove(activeClass);
-						}
-					});
-				});
-			});
-		});
-	}
+            childs.forEach((current) => {
+                current.addEventListener('click', () => {
+                    childs.forEach((item) => {
+                        if (current === item && !item.classList.contains('active')) {
+                            current.classList.add('active');
+                        } else {
+                            item.classList.remove('active');
+                        }
+                    });
+                });
+            });
+        });
+    }
 };
 
 document.addEventListener('DOMContentLoaded', setCloseSelects);
